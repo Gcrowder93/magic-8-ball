66630cddd1650cfbcc97f6ab0aa77a1d54dfdb16
Chase <Gcrowder93@gmail.com>
2021-10-05 13:21:21 -0700

    added an image, button, and h1

---
+++ answers.js
@@ -0,0 +1,9 @@
+export const answers= [
+    'Yes',
+    'No',
+    'Maybe',
+    'Unlikely',
+    'You cannot be serious',
+    'Dont count on it',
+    'More than likely',
+  ];
--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-// import functions and grab DOM elements
-
-// initialize global state
-
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+  import { answers } from './answers.js';
+
+const colors = ['red', 'white', 'blue'];
+
+const getRandomNumber = (max)=>{
+  return Math.floor(Math.random() * max);
+}
+
+const randomAnswer = getRandomNumber(answers.length);
+console.log(answers[randomAnswer]);
+
+
+
+
+//html elements = paragraph for question, button for submit, 
+//states = answer; randomIdx
+
+//using this to create random answers for magic 8 ball
+//input to ask a question
+//button to ask for random answer
+//maybe add shake effects
+//style. fonts, centering, shape of box(triangle), etc
+//button click event listener, generate random index, get value of answer, display answer on page.
+
+
+
+
+//have answers in seperate file, then import into apps.js
